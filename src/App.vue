1a3d1a2e1d7cb0a8964dd7a772789142e39252a7
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-09-09 12:58:51 +0500

    Vue components using TS

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="app">
-    Hello, hyrule
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Jahanzeb')">change name</button>
+    <button @click="changeAge(30)">change age</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+      return age;
+    },
+  },
 });
 </script>
 
 <style>
-
 </style>
